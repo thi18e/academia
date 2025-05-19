89bff7d1f1850f9919bd420eb8e713f5b5ef25df
thi18e <thiagotlcs@gmail.com>
2025-05-19 15:29:24 -0300

    Atualização geral

---
+++ includes/navbaragendamento.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="../public/home.php">Academia</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+     
+        <li class="nav-item">
+          <a class="nav-link" href="marcarAgendamento.php">Agendar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="agendamentos.php">Meus Agendamentos</a>
+        </li>
+        <li class="nav-item">
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div style="height: 70px;"></div>
